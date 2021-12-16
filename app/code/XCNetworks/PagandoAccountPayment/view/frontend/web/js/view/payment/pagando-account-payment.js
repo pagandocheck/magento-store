23b55fa9f2a7e64c26833e6cae043b0dca986c4e
Karen Villa <kvilla@blacklabs.mx>
2021-12-16 13:12:20 -0600

    Se agrega componente para el nuevo metodo de pago.

---
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/pagando-account-payment.js
@@ -0,0 +1,16 @@
+define([
+    'uiComponent',
+    'Magento_Checkout/js/model/payment/renderer-list'
+], function (Component, rendererList) {
+    'use strict';
+
+    rendererList.push(
+        {
+            type: 'pagandoAccountPayment',
+            component: 'XCNetworks_PagandoPayment/js/view/payment/method-renderer/pagandoAccountPayment-method'
+        }
+    );
+
+    /** Add view logic here if needed */
+    return Component.extend({});
+});
