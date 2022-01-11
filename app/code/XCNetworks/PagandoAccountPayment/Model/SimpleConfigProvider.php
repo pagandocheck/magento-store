ec5e8ac2b8749efb7ac28a35898ceb9f9ab0e2e6
Karen Villa <kvilla@blacklabs.mx>
2022-01-11 10:12:34 -0600

    Se modifica el configProvider

--- app/code/XCNetworks/PagandoAccountPayment/Model/SimpleConfigProvider.php
+++ app/code/XCNetworks/PagandoAccountPayment/Model/SimpleConfigProvider.php
@@ -21,6 +21,51 @@ use XCNetworks\PagandoAccountPayment\Model\PagandoAccountPayment;
 class SimpleConfigProvider implements ConfigProviderInterface
 {
 
+    protected $methodCode = 'pagandoAccountPayment';
+    protected $_scopeConfig;
+    protected $_methodInstance;
+    protected $_checkoutSession;
+    protected $_assetRepo;
+    protected $_productMetaData;
+    protected $_coreHelper;
+    protected $_context;
+    protected $_paymentFactory;
+    protected $logger;
+    protected $countries;
+
+    /**
+     * SimpleConfigProvider constructor.
+     * @param Context $context
+     * @param PaymentHelper $paymentHelper
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Session $checkoutSession
+     * @param Repository $assetRepo
+     * @param ProductMetadataInterface $productMetadata
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function __construct(
+        Context $context,
+        Repository $assetRepo,
+        Session $checkoutSession,
+        PaymentHelper $paymentHelper,
+        ScopeConfigInterface $scopeConfig,
+        ProductMetadataInterface $productMetadata,
+        PagandoPayment $paymentFactory,
+        \Psr\Log\LoggerInterface $customLogger
+
+    )
+    {
+        $this->_context = $context;
+        $this->_assetRepo = $assetRepo;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_methodInstance = $paymentHelper->getMethodInstance($this->methodCode);
+        $this->_checkoutSession = $checkoutSession;
+        $this->_productMetaData = $productMetadata;
+        $this->_paymentFactory = $paymentFactory;
+        $this->logger = $customLogger;
+
+    }
+
     public function getStoredCards(){
       $result = array();
       $result['0'] = "Test";
