7c9bfab885b563ef13a52076be2cc31dbb843816
Karen Villa <kvilla@blacklabs.mx>
2022-02-14 10:49:41 -0600

    Valores para las promociones

--- app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
@@ -186,7 +186,7 @@
 
                 var request = $.ajax({
                     method: "POST",
-                    url: "https://5bce-187-227-75-190.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
+                    url: "https://f2e3-2806-104e-4-15d4-b0b4-e80b-2acc-2c44.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
                     headers: {
                         "Content-Type": "'Content-Type': 'application/x-www-form-urlencoded'",
                         "Authorization": "Bearer "+jwt_token
