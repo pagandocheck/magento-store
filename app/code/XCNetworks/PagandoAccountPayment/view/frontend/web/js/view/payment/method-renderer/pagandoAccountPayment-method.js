ff22fcc4112d781532ac3d18aa9102112e1326e9
Karen Villa <kvilla@blacklabs.mx>
2022-02-25 08:26:53 -0600

    Se agregan valores para la orden de pago

--- app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
@@ -215,7 +215,7 @@
                      //   "Authorization": `Bearer ${jwt_token}`,
                     //         "Access-Control-Allow-Origin": "https://44dc-2806-104e-4-15d4-b0b4-e80b-2acc-2c44.ngrok.io"
                     //},
-                    url: "https://409f-2806-104e-4-f559-4517-7336-18fa-70e3.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
+                    url: "https://da43-2806-104e-4-f559-4517-7336-18fa-70e3.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
                     dataType: 'json',
                     data: payload,
                     crossDomain: true
@@ -287,7 +287,7 @@
                 method: "POST",
                 type: "POST",
                 withCredentials: true,
-                url: "https://409f-2806-104e-4-f559-4517-7336-18fa-70e3.ngrok.io/v1/pagando/orders/create-order",
+                url: "https://da43-2806-104e-4-f559-4517-7336-18fa-70e3.ngrok.io/v1/pagando/orders/create-order",
                 dataType: 'json',
                 data: payload,
                 crossDomain: true
@@ -304,7 +304,7 @@
                 console.log("EXITOOOO2");
                 const data= request.responseJSON.data;
                 self.messageContainer.addSuccessMessage({'message': 'Your payment with Pagando is complete.'});
-                window.location.replace(url.build('checkout/onepage/success'));
+                // window.location.replace(url.build('checkout/onepage/success'));
 
             });
 
