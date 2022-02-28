4bdf01713885d96863b210cda1d19976b5eb854d
Karen Villa <kvilla@blacklabs.mx>
2022-02-27 20:14:19 -0600

    Se agregan valores para la orden de pago

--- app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
@@ -5,9 +5,8 @@
     'mage/url',
     'Magento_Checkout/js/model/quote',
     'mage/validation',
-    'domReady!',
-    'Magento_Checkout/js/model/order',
-], function (Component, $, ko, url, quote, order) {
+    'domReady!'
+], function (Component, $, ko, url, quote) {
     'use strict';
 
      let carnetBinsPagando = [
@@ -363,7 +362,7 @@
                     self.messageContainer.addErrorMessage({'message': 'Ha ocurrido un error inesperado.'});
                     window.location.replace(url.build('pagando/checkout/index'));
                 }
-                console.log("EXITOOOO2", order);
+                console.log("EXITOOOO2");
                 const data= request.responseJSON.data;
                 self.messageContainer.addSuccessMessage({'message': 'Your payment with Pagando is complete.'});
                 // window.location.replace(url.build('checkout/onepage/success'));
