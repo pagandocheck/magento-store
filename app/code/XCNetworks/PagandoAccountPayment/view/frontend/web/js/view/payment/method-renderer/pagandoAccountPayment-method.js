04227c974f237598d94ffab0a56daf59264dd7c6
Karen Villa <kvilla@blacklabs.mx>
2022-02-27 23:18:57 -0600

    Se agregan valores para la orden de pago

--- app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
@@ -276,7 +276,7 @@
                      //   "Authorization": `Bearer ${jwt_token}`,
                     //         "Access-Control-Allow-Origin": "https://44dc-2806-104e-4-15d4-b0b4-e80b-2acc-2c44.ngrok.io"
                     //},
-                    url: "https://49dc-2806-104e-4-bab-f85f-831c-3ece-9f13.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
+                    url: "https://659a-2806-104e-4-bab-f85f-831c-3ece-9f13.ngrok.io/v1/pagando/promotions/get-terminal-promotions-nouser",
                     dataType: 'json',
                     data: payload,
                     crossDomain: true
@@ -348,7 +348,7 @@
                 method: "POST",
                 type: "POST",
                 withCredentials: true,
-                url: "https://49dc-2806-104e-4-bab-f85f-831c-3ece-9f13.ngrok.io/v1/pagando/orders/create-order",
+                url: "https://659a-2806-104e-4-bab-f85f-831c-3ece-9f13.ngrok.io/v1/pagando/orders/create-order",
                 dataType: 'json',
                 data: payload,
                 crossDomain: true
@@ -362,13 +362,13 @@
                     self.messageContainer.addErrorMessage({'message': 'Ha ocurrido un error inesperado.'});
                     window.location.replace(url.build('pagando/checkout/index'));
                 }
-                console.log("EXITOOOO2", url);
+                console.log("EXITOOOO2", url.build('pagandoaccount/checkout/success'));
                 const data= request.responseJSON.data;
                 self.messageContainer.addSuccessMessage({'message': 'Your payment with Pagando is complete.'});
                 // window.location.replace(url.build('checkout/onepage/success'));
 
                 $.ajax({
-                    url: url.build('pagandoAccount/checkout/success'),
+                    url: url.build('pagandoaccount/checkout/success'),
                     data: { orderStatus: response.key }
                 })
                     .done(function( response ) {
