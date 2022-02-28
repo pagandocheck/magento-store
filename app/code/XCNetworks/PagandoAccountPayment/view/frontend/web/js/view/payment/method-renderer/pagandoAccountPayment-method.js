dd016be337fed340f7e5fb2402098da329d4320e
Karen Villa <kvilla@blacklabs.mx>
2022-02-27 18:06:16 -0600

    Se agregan valores para la orden de pago

--- app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
+++ app/code/XCNetworks/PagandoAccountPayment/view/frontend/web/js/view/payment/method-renderer/pagandoAccountPayment-method.js
@@ -368,7 +368,7 @@
                 // window.location.replace(url.build('checkout/onepage/success'));
                 $.ajax({
                     method: "POST",
-                    url: "XCNetworks/PagandoPayment/Controller/Checkout/processResponse.php",
+                    url: "pagandoaccount/checkout/success",
                     data: { orderStatus: response.key }
                 })
                     .done(function( response ) {
