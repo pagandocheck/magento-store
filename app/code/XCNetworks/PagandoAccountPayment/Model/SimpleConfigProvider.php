fe58e3624a726e14fd450794a3ded163afa1fc79
Karen Villa <kvilla@blacklabs.mx>
2022-03-04 00:02:04 -0600

    Se agregan valores para la orden de pago

--- app/code/XCNetworks/PagandoAccountPayment/Model/SimpleConfigProvider.php
+++ app/code/XCNetworks/PagandoAccountPayment/Model/SimpleConfigProvider.php
@@ -88,7 +88,7 @@ class SimpleConfigProvider implements ConfigProviderInterface
                 $data = [
                     'payment' => [
                         $this->methodCode => [
-                            'jwt_token' => $this->getToken()
+                            'jwt_token' => 'jhbgfcdxcgvb'
                         ],
                     ],
                 ];
